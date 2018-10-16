7bfac4c7b0f4581d25baa2591724eccac64e2733
baibing <baibing@sit.com.cn>
2018-10-16 10:51:44 +0800

    添加日志切面类

---
+++ src/main/java/com/sailing/springbootmybatis/common/log/aop/LogAspect.java
@@ -0,0 +1,141 @@
+package com.sailing.springbootmybatis.common.log.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.sailing.springbootmybatis.bean.SysLog;
+import com.sailing.springbootmybatis.common.log.annotation.MyLog;
+import com.sailing.springbootmybatis.common.response.ResponseData;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+
+/**
+ * @author baibing
+ * @project: springboot-mybatis
+ * @package: com.sailing.springbootmybatis.common.log.aop
+ * @Description: 日志切面处理类
+ * @date 2018/10/15 16:11
+ */
+@Aspect//声明一个切面
+@Component//必须添加此注解，可以让spring获取此切面
+public class LogAspect {
+
+    @Pointcut("@annotation(com.sailing.springbootmybatis.common.log.annotation.MyLog)")
+    public void logPointCut(){
+
+    }
+
+    /**
+     *   运行顺序：
+     *   环绕通知开始
+         前置通知
+         环绕通知结束
+         后置通知
+         方法返回后调用
+     */
+//    @Before("logPointCut()")
+//    public void testBefore(){
+//        System.out.println("前置通知");
+//    }
+//
+//    @After("logPointCut()")
+//    public void testAfter(){
+//        System.out.println("后置通知");
+//    }
+
+//    @Around("logPointCut()")
+//    public Object testAround(ProceedingJoinPoint joinPoint){
+//        System.out.println("环绕通知开始");
+//        Object result = null;
+//        try {
+//            result = joinPoint.proceed();//执行目标方法
+//            System.out.println(((ResponseData)result).getData());
+//        } catch (Throwable throwable) {
+//            throwable.printStackTrace();
+//        }
+//        System.out.println("环绕通知结束");
+//        return result;
+//    }
+
+    @AfterReturning("logPointCut()")
+    public void testAfterReturning(JoinPoint joinPoint){
+        SysLog log = new SysLog();
+
+        //从切面织入点处通过反射机制获取织入点处的方法
+        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        Method method = signature.getMethod();
+
+        MyLog myLog = method.getAnnotation(MyLog.class);
+        if(myLog != null){
+            System.out.println("注解值:" + myLog.value());
+        }
+
+        //获取请求的类名
+        String className = joinPoint.getTarget().getClass().getName();
+        System.out.println("类名:" + className);
+
+        //获取请求的方法名
+        String methodName = method.getName();
+        System.out.println("方法名:" + methodName);
+
+        //请求的参数
+        Object[] args = joinPoint.getArgs();
+        String params = JSON.toJSONString(args);
+        System.out.println("方法参数:" + params);
+
+        //获取用户ip地址
+        HttpServletRequest request = getRequest(joinPoint);
+        System.out.println("request:" + request);
+
+        String ip = getIpAddr(request);
+        System.out.println("ip地址:" + ip);
+    }
+
+    /**
+     * 获取参数request
+     * @param point
+     * @return
+     */
+    private HttpServletRequest getRequest(JoinPoint point) {
+        Object[] args = point.getArgs();
+        for (Object obj : args) {
+            if (obj instanceof HttpServletRequest)
+                return (HttpServletRequest) obj;
+        }
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+        return request;
+    }
+
+    /**
+     * 获取访问者ip地址
+     * @param request
+     * @return
+     */
+    public String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+//    @AfterThrowing("logPointCut()")
+//    public void testAfterThrowing(){
+//        System.out.println("方法抛出异常后调用");
+//    }
+}
