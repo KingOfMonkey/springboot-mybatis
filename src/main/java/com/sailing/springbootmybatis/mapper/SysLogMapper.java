c65ad491979afee7f0aebe0abf151ed4379823c0
baibing <baibing@sit.com.cn>
2018-10-16 15:50:49 +0800

    添加日志mapper接口

---
+++ src/main/java/com/sailing/springbootmybatis/mapper/SysLogMapper.java
@@ -0,0 +1,17 @@
+package com.sailing.springbootmybatis.mapper;
+
+import com.sailing.springbootmybatis.bean.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author baibing
+ * @project: springboot-mybatis
+ * @package: com.sailing.springbootmybatis.mapper
+ * @Description: 日志mapper文件
+ * @date 2018/10/16 14：17
+ */
+@Mapper
+public interface SysLogMapper {
+
+    int insert(SysLog log);
+}
