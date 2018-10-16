0c4e584acf6d3d52b3cfc7c173ed7edd01744fbb
baibing <baibing@sit.com.cn>
2018-10-16 11:29:07 +0800

    添加日志操作枚举类

---
+++ src/main/java/com/sailing/springbootmybatis/common/log/LogOperationEnum.java
@@ -0,0 +1,18 @@
+package com.sailing.springbootmybatis.common.log;
+
+/**
+ * @author Lenovo
+ * @project: springboot-mybatis
+ * @package: com.sailing.springbootmybatis.common.log
+ * @Description:
+ * @date 2018/10/161058
+ */
+public enum LogOperationEnum {
+    SELECT,
+    INSERT,
+    UPDATE,
+    DELETE;
+
+    private LogOperationEnum(){
+    }
+}
