83f44c460fb34087e9059a4ceec40c776761a50b
baibing <baibing@sit.com.cn>
2018-10-16 15:49:27 +0800

    修改注释

--- src/main/java/com/sailing/springbootmybatis/common/log/LogOperationEnum.java
+++ src/main/java/com/sailing/springbootmybatis/common/log/LogOperationEnum.java
@@ -1,11 +1,11 @@
 package com.sailing.springbootmybatis.common.log;
 
 /**
- * @author Lenovo
+ * @author baibing
  * @project: springboot-mybatis
  * @package: com.sailing.springbootmybatis.common.log
- * @Description:
- * @date 2018/10/161058
+ * @Description: 操作类型枚举类
+ * @date 2018/10/16 10：58
  */
 public enum LogOperationEnum {
     SELECT,
