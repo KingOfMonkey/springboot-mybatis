adab01b81d2b1febb49a6a5d776a53e7c5431828
baibing <baibing@sit.com.cn>
2018-12-07 11:29:45 +0800

    增加token验证返回状态码和信息枚举

--- src/main/java/com/sailing/springbootmybatis/common/response/ResponseEnum.java
+++ src/main/java/com/sailing/springbootmybatis/common/response/ResponseEnum.java
@@ -12,7 +12,10 @@ public enum ResponseEnum {
     SUCCESS(1, "请求成功"),//接口正常返回
     FAIL(0, "请求失败"),//请求失败，一般是参数校验没有通过
     SERVICE_EXCEPTION(21000, "业务异常"),//业务层异常
-    SERVER_ERROR(51000, "程序异常");//程序异常，一般是bug，需要排查解决
+    SERVER_ERROR(51000, "程序异常"),//程序异常，一般是bug，需要排查解决
+    NOT_LOGIN(31000, "您尚未登陆，无调用接口权限"),
+    REQUEST_ILLEGAL(41000, "非法请求：token验证未通过"),
+    TOKEN_EXPIRED(42000,"请求未受理：token令牌已经过期");
 
     private Integer code;
 
