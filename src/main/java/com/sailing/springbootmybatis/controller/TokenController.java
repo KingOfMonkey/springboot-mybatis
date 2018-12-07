86b679bd3a2acb83d747ceb369524f1bd3602c73
baibing <baibing@sit.com.cn>
2018-12-07 11:30:19 +0800

    修改token测试controller

--- src/main/java/com/sailing/springbootmybatis/controller/TokenController.java
+++ src/main/java/com/sailing/springbootmybatis/controller/TokenController.java
@@ -1,19 +1,11 @@
 package com.sailing.springbootmybatis.controller;
 
+import com.sailing.springbootmybatis.service.TokenService;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author baibing
@@ -25,46 +17,19 @@ import java.util.Map;
 @RestController
 public class TokenController {
 
-    @RequestMapping(value = "/token/getToken", method = RequestMethod.GET)
-    public String createToken(String id, String subject, long ttlMillis){
-        // 指定签名的时候使用的签名算法，也就是header那部分，jjwt已经将这部分内容封装好了。
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-        // 生成JWT的时间
-        Long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-        // 创建payload的私有声明（根据特定的业务需要添加，如果要拿这个做验证，一般是需要和jwt的接收方提前沟通好验证方式的）
-        Map<String,Object> claims = new HashMap<String,Object>();
-        claims.put("uid", "oI2_M4shnL1eMApcVJ3IQiYd0_qk");
-        claims.put("user_name", "baibing");
-        claims.put("nick_name","白冰");
-        // 生成签名的时候使用的秘钥secret，切记这个秘钥不能外露哦。它就是你服务端的私钥，在任何场景都不应该流露出去。
-        SecretKey key = generalKey();
-        //下面就是在为payload添加各种标准声明和私有声明了
-        JwtBuilder builder = Jwts.builder()//这里其实就是new一个JwtBuilder，设置jwt的body
-                .setClaims(claims)//如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
-                .setId(id)//设置jti(JWT ID)：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，主要用来作为一次性token,从而回避重放攻击
-                .setIssuedAt(now)//iat: jwt的签发时间
-                .setSubject(subject)//sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
-                .signWith(signatureAlgorithm, key);//设置签名使用的签名算法和签名使用的秘钥
-        if (ttlMillis >= 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            builder.setExpiration(exp);     //设置过期时间
-        }
-        return builder.compact();
-    }
+    @Autowired
+    private TokenService tokenService;
 
     /**
-     * 由字符串生成加密key
+     * 生成jwt
+     * @param id
+     * @param subject
+     * @param ttlMillis
      * @return
      */
-    public SecretKey generalKey(){
-        String stringKey = "7786df7fc3a34e26a61c034d5ec8245d";//本地配置文件中加密的密文
-        byte[] encodedKey = Base64.decodeBase64(stringKey);//本地的密码解码[B@152f6e2
-        System.out.println(encodedKey);//[B@152f6e2
-        System.out.println(Base64.encodeBase64URLSafeString(encodedKey));//7786df7fc3a34e26a61c034d5ec8245d
-        SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");// 根据给定的字节数组使用AES加密算法构造一个密钥，使用 encodedKey中的始于且包含 0 到前 leng 个字节这是当然是所有。（后面的文章中马上回推出讲解Java加密和解密的一些算法）
-        return key;
+    @RequestMapping(value = "/token/getToken", method = RequestMethod.GET)
+    public String createToken(String id, String subject, long ttlMillis){
+        return tokenService.createJWT(id,subject,ttlMillis);
     }
 
     /**
@@ -75,10 +40,6 @@ public class TokenController {
      */
     @RequestMapping(value = "/token/verification", method = RequestMethod.POST)
     public Claims parseJWT(String jwt) throws Exception{
-        SecretKey key = generalKey();  //签名秘钥，和生成的签名的秘钥一模一样
-        Claims claims = Jwts.parser()  //得到DefaultJwtParser
-                .setSigningKey(key)         //设置签名的秘钥
-                .parseClaimsJws(jwt).getBody();//设置需要解析的jwt
-        return claims;
+        return tokenService.parseJWT(jwt);
     }
 }
