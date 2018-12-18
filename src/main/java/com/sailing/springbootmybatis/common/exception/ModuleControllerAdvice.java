d50357f6e776b0fbbc6b54484cf3889b112d5adc
baibing <baibing@sit.com.cn>
2018-12-18 18:05:51 +0800

    修改全局异常输出模式，增加异常堆栈信息输出

--- src/main/java/com/sailing/springbootmybatis/common/exception/ModuleControllerAdvice.java
+++ src/main/java/com/sailing/springbootmybatis/common/exception/ModuleControllerAdvice.java
@@ -21,8 +21,16 @@ public class ModuleControllerAdvice {
 
     private Logger SERVICE_LOGGER = LoggerFactory.getLogger(ServiceException.class);
 
-    private Logger SERVER_LOGGER = LoggerFactory.getLogger(Exception.class);
+    private Logger SERVER_LOGGER = LoggerFactory.getLogger(ModuleControllerAdvice.class);
 
+
+    /**
+     *  Logger类下有多个不同的error方法，根据传入参数的个数及类型的不同，自动选择不同的重载方法。
+     * 当error(Object obj)只传入一个参数时会将异常对象作为Object使用，并最终当做String打印出来，
+     * 当使用两个参数error(String message, Throwable t)，且第二个参数为Throwable时，才会将完整的异常堆栈打印出来。
+     * @param e
+     * @return
+     */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     private ResponseData handleServiceException(Exception e){
@@ -30,12 +38,11 @@ public class ModuleControllerAdvice {
         ResponseData responseData;
         if(e instanceof ServiceException){
             message = "业务受理失败, 原因:" + e.getLocalizedMessage();
-            SERVICE_LOGGER.info("业务受理失败, 原因:{}", e.getLocalizedMessage());
+            SERVICE_LOGGER.error("业务受理失败, 原因:{}", e.getLocalizedMessage(), e);
             responseData = BuildResponseUtil.buildServiceFailResponse(message);
         }else{
-            e.printStackTrace();
-            message = "程序异常, 信息:" + e;
-            SERVER_LOGGER.info("程序异常, 信息:{}", e);
+            message = "程序异常, 信息:" + e.getLocalizedMessage();
+            SERVER_LOGGER.error("程序异常, 详细信息:{}", e.getLocalizedMessage() , e);
             responseData = BuildResponseUtil.buildServerErrorResponse(message);
         }
         return responseData;
