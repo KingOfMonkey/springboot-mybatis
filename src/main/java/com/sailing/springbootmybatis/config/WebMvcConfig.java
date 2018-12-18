4b2e08a4a0eef02ef8c546f73c612f321df47443
baibing <baibing@sit.com.cn>
2018-12-18 18:06:54 +0800

    修复swagger-ui.html 404问题

--- src/main/java/com/sailing/springbootmybatis/config/WebMvcConfig.java
+++ src/main/java/com/sailing/springbootmybatis/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.sailing.springbootmybatis.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -21,5 +22,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/client").setViewName("client");
         registry.addViewController("/index").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
